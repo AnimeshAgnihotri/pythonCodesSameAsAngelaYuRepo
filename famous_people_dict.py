df9b7358c0eef8e81c330917e32f51c3f41c96bc
Animesh Agnihotri <animeshagnihotri623@outlook.com>
2026-06-14 18:05:20 +0530

    Add famous people Instagram follower data

---
+++ famous_people_dict.py
@@ -0,0 +1,103 @@
+famous_people_instagram = {
+
+    "Cristiano Ronaldo": {"followers": 664_000_000, "work": "Footballer"},
+
+    "Lionel Messi": {"followers": 506_000_000, "work": "Footballer"},
+
+    "Selena Gomez": {"followers": 406_000_000, "work": "Singer and Actress"},
+
+    "Dwayne Johnson": {"followers": 383_000_000, "work": "Actor and Wrestler"},
+
+    "Kylie Jenner": {"followers": 383_000_000, "work": "Media Personality and Entrepreneur"},
+
+    "Ariana Grande": {"followers": 363_000_000, "work": "Singer and Actress"},
+
+    "Kim Kardashian": {"followers": 345_000_000, "work": "Media Personality and Businesswoman"},
+
+    "Beyonce": {"followers": 300_000_000, "work": "Singer and Actress"},
+
+    "Khloe Kardashian": {"followers": 293_000_000, "work": "Media Personality"},
+
+    "Justin Bieber": {"followers": 287_000_000, "work": "Singer"},
+
+    "Kendall Jenner": {"followers": 279_000_000, "work": "Model and Media Personality"},
+
+    "Taylor Swift": {"followers": 273_000_000, "work": "Singer-Songwriter"},
+
+    "Virat Kohli": {"followers": 273_000_000, "work": "Cricketer"},
+
+    "Jennifer Lopez": {"followers": 240_000_000, "work": "Singer and Actress"},
+
+    "Neymar Jr": {"followers": 230_000_000, "work": "Footballer"},
+
+    "Miley Cyrus": {"followers": 206_000_000, "work": "Singer and Actress"},
+
+    "Katy Perry": {"followers": 196_000_000, "work": "Singer"},
+
+    "Zendaya": {"followers": 174_000_000, "work": "Actress and Singer"},
+
+    "Kevin Hart": {"followers": 172_000_000, "work": "Comedian and Actor"},
+
+    "Cardi B": {"followers": 161_000_000, "work": "Rapper and Actress"},
+
+    "LeBron James": {"followers": 154_000_000, "work": "Basketball Player"},
+
+    "Demi Lovato": {"followers": 148_000_000, "work": "Singer and Actress"},
+
+    "Rihanna": {"followers": 145_000_000, "work": "Singer and Entrepreneur"},
+
+    "Chris Brown": {"followers": 140_000_000, "work": "Singer"},
+
+    "Drake": {"followers": 139_000_000, "work": "Rapper and Singer"},
+
+    "Ellen DeGeneres": {"followers": 130_000_000, "work": "TV Host and Comedian"},
+
+    "Kylian Mbappe": {"followers": 130_000_000, "work": "Footballer"},
+
+    "Billie Eilish": {"followers": 124_000_000, "work": "Singer"},
+
+    "Lisa": {"followers": 106_000_000, "work": "Singer and Dancer"},
+
+    "Gal Gadot": {"followers": 104_000_000, "work": "Actress"},
+
+    "Vin Diesel": {"followers": 103_000_000, "work": "Actor"},
+
+    "Narendra Modi": {"followers": 101_000_000, "work": "Prime Minister of India"},
+
+    "Shraddha Kapoor": {"followers": 95_000_000, "work": "Actress"},
+
+    "Snoop Dogg": {"followers": 89_000_000, "work": "Rapper"},
+
+    "David Beckham": {"followers": 88_000_000, "work": "Former Footballer"},
+
+    "Priyanka Chopra": {"followers": 87_000_000, "work": "Actress and Producer"},
+
+    "Shakira": {"followers": 85_000_000, "work": "Singer"},
+
+    "Emma Watson": {"followers": 74_000_000, "work": "Actress"},
+
+    "Will Smith": {"followers": 69_000_000, "work": "Actor"},
+
+    "Jacqueline Fernandez": {"followers": 68_000_000, "work": "Actress"},
+
+    "Akshay Kumar": {"followers": 67_000_000, "work": "Actor"},
+
+    "Tom Holland": {"followers": 66_000_000, "work": "Actor"},
+
+    "Alia Bhatt": {"followers": 66_000_000, "work": "Actress"},
+
+    "Anushka Sharma": {"followers": 65_000_000, "work": "Actress"},
+
+    "Robert Downey Jr": {"followers": 58_000_000, "work": "Actor"},
+
+    "Deepika Padukone": {"followers": 80_000_000, "work": "Actress"},
+
+    "Sachin Tendulkar": {"followers": 50_000_000, "work": "Former Cricketer"},
+
+    "Zlatan Ibrahimovic": {"followers": 63_000_000, "work": "Footballer"},
+
+    "Salman Khan": {"followers": 70_000_000, "work": "Actor"},
+
+    "Hrithik Roshan": {"followers": 48_000_000, "work": "Actor"}
+
+}
